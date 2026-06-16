bcd4fb69c408f85cd821ccb2785639f4fcdd0463
Zunaid Akhtar <akhtarzunaid24@gmail.com>
2026-06-17 00:46:27 +0530

    Optimised the code

--- tetris.cpp
+++ tetris.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <ctime>
 #include <cstdlib>
+using namespace sf;
+using namespace std;
 
 const int M = 20;
 const int N = 10;
@@ -29,28 +31,24 @@ bool check() {
 }
 
 int main(int argc, char* argv[]) {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    srand(static_cast<unsigned int>(time(nullptr)));
+    RenderWindow window(VideoMode({320, 480}), "TETRIS");
 
-    // SFML 3 Window Initialization using Vector2u
-    sf::RenderWindow window(sf::VideoMode({320, 480}), "TETRIS");
-
-    sf::Texture tex1, tex2, tex3;
-    // Warnings are handled naturally by letting files load sequentially
+    Texture tex1, tex2, tex3;
     tex1.loadFromFile("images/tiles.png");
     tex2.loadFromFile("images/background.png");
     tex3.loadFromFile("images/frame.png");
 
-    sf::Sprite s(tex1), background(tex2), frame(tex3);
+    Sprite s(tex1), background(tex2), frame(tex3);
 
     int dx = 0;
     bool rotate = false;
     int colorNum = 1;
     float timer = 0;
     float delay = 0.3f;
-    sf::Clock clock;
+    Clock clock;
 
-    // Initialize first tetromino piece on startup so 'a' array isn't empty
-    int firstPiece = std::rand() % 7;
+    int firstPiece = rand() % 7;
     for (int i = 0; i < 4; i++) {
         a[i].x = figures[firstPiece][i] % 2 + N / 2 - 1;
         a[i].y = figures[firstPiece][i] / 2;
@@ -61,29 +59,26 @@ int main(int argc, char* argv[]) {
         clock.restart();
         timer += time;
 
-        // --- MODERN SFML 3 EVENT LOOP ---
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>()) {
+        while (const optional event = window.pollEvent()) {
+            if (event->is<Event::Closed>()) {
                 window.close();
             }
 
-            if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-                if (keyPressed->code == sf::Keyboard::Key::Up) {
+            if (const auto* keyPressed = event->getIf<Event::KeyPressed>()) {
+                if (keyPressed->code == Keyboard::Key::Up) {
                     rotate = true;
-                } else if (keyPressed->code == sf::Keyboard::Key::Left) {
+                } else if (keyPressed->code == Keyboard::Key::Left) {
                     dx = -1;
-                } else if (keyPressed->code == sf::Keyboard::Key::Right) {
+                } else if (keyPressed->code == Keyboard::Key::Right) {
                     dx = 1;
                 }
             }
         }
 
-        // Real-time button holding check
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+        if (Keyboard::isKeyPressed(Keyboard::Key::Down)) {
             delay = 0.05f;
         }
 
-        // Horizontal Movement
         for (int i = 0; i < 4; i++) {
             b[i] = a[i];
             a[i].x += dx;
@@ -92,9 +87,8 @@ int main(int argc, char* argv[]) {
             for (int i = 0; i < 4; i++) a[i] = b[i];
         }
 
-        // Rotation
         if (rotate) {
-            Point p = a[1]; // Center of rotation (Using index 1 instead of invalid 'i')
+            Point p = a[1]; 
             for (int i = 0; i < 4; i++) {
                 int x = a[i].y - p.y;
                 int y = a[i].x - p.x;
@@ -106,7 +100,6 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        // Tick / Falling Logic
         if (timer > delay) {
             for (int i = 0; i < 4; i++) {
                 b[i] = a[i];
@@ -117,17 +110,16 @@ int main(int argc, char* argv[]) {
                     field[b[i].y][b[i].x] = colorNum;
                 }
 
-                colorNum = 1 + std::rand() % 7;
-                int n = std::rand() % 7;
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
                 for (int i = 0; i < 4; i++) {
-                    a[i].x = figures[n][i] % 2 + N / 2 - 1; // Spawns near horizontal center
+                    a[i].x = figures[n][i] % 2 + N / 2 - 1; 
                     a[i].y = figures[n][i] / 2;
                 }
             }
             timer = 0;
         }
 
-        // Line Clearing Logic
         int k = M - 1;
         for (int i = M - 1; i > 0; i--) {
             int count = 0;
@@ -138,29 +130,25 @@ int main(int argc, char* argv[]) {
             if (count < N) k--;
         }
 
-        // Reset tracking vars
         dx = 0;
         rotate = false;
         delay = 0.3f;
 
-        // --- DRAWING STAGE (Modern SFML 3 Coordinate Vectors) ---
-        window.clear(sf::Color::White);
+        window.clear(Color::White);
         window.draw(background);
 
-        // Draw Field
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 if (field[i][j] == 0) continue;
-                s.setTextureRect(sf::IntRect({field[i][j] * 18, 0}, {18, 18}));
+                s.setTextureRect(IntRect({field[i][j] * 18, 0}, {18, 18}));
                 s.setPosition({static_cast<float>(j * 18), static_cast<float>(i * 18)});
                 s.move({28.f, 31.f});
                 window.draw(s);
             }
         }
 
-        // Draw Active falling piece
         for (int i = 0; i < 4; i++) {
-            s.setTextureRect(sf::IntRect({colorNum * 18, 0}, {18, 18}));
+            s.setTextureRect(IntRect({colorNum * 18, 0}, {18, 18}));
             s.setPosition({static_cast<float>(a[i].x * 18), static_cast<float>(a[i].y * 18)});
             s.move({28.f, 31.f});
             window.draw(s);
